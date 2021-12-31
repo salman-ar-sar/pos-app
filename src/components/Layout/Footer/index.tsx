fbb85d25d2afb9a207256bea754993f0eb1bd7b5
Salman A <salman.a@hatio.in>
2021-12-31 23:37:59 +0530

    Update footer FC and add styles

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./index.scss";
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      Copyright© 2022 P.O.S. Limited • All Rights Reserved
+    </footer>
+  );
 };
 
 export default Footer;
