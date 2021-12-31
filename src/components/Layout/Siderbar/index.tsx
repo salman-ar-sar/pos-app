775f3b492791b4992ca2498f2f285d7100aa3107
Salman A <salman.a@hatio.in>
2021-12-31 23:38:16 +0530

    Update Sidebar FC and add styles

--- src/components/Layout/Siderbar/index.tsx
+++ src/components/Layout/Siderbar/index.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./index.scss";
 
 const Sidebar: React.FC = () => {
+  interface SideBarItemType {
+    link: string;
+    name: string;
+  }
+
+  const sideBarItems: SideBarItemType[] = [
+    { link: "/home", name: "Home" },
+    { link: "/bills", name: "View Bills" },
+    { link: "/bills/new", name: "Create new bill" },
+  ];
+
   return (
-    <div className="sidebar-container">
-      <ul>
-        <li>
-          <NavLink to="/home" className="selected-item">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/bills/new" className="selected-item">
-            New Bill
-          </NavLink>
-        </li>
+    <nav className="sidebar-container">
+      <ul className="sidebar-list">
+        {sideBarItems.map((item) => (
+          <li className="list-elem">
+            <NavLink
+              to={item.link}
+              className={(navData) => (navData.isActive ? "selected-link" : "")}
+              end
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
-    </div>
+    </nav>
   );
 };
 
