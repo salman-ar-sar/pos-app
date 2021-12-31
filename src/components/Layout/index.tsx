e29193b1ad859b6651444ec37b4c07c5b5fd91d5
Salman A <salman.a@hatio.in>
2021-12-31 23:37:29 +0530

    Update layout styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import "./index.scss";
 import Sidebar from "./Siderbar";
+import "./index.scss";
 
 const Layout: React.FC = () => {
   return (
