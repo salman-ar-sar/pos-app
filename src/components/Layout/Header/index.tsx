5967aad3dde4aa31cf66164aa815eb5256357090
Salman A <salman.a@hatio.in>
2021-12-31 23:37:46 +0530

    Update header FC and add styles

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ReactComponent as HeaderLogo } from "./header-logo.svg";
+import "./index.scss";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <HeaderLogo className="logo" />
+      <div className="main-heading">MyPOS</div>
+    </header>
+  );
 };
 
 export default Header;
