2c6ddc90ff67f8a15ff94477b0a8d3e2c3e0e8a2
Salman A <salman.a@hatio.in>
2021-12-31 21:59:07 +0530

    Update Layout - Add sidebar - Use new Outlet feature from router v6

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,20 @@
-import React, { ReactNode } from "react";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./index.scss";
+import Sidebar from "./Siderbar";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+const Layout: React.FC = () => {
   return (
     <div className="layout-container">
       <Header />
-      <div className="layout-content">{children}</div>
+      <div className="layout-content">
+        <Sidebar />
+        <div className="content-container">
+          <Outlet />
+        </div>
+      </div>
       <Footer />
     </div>
   );
